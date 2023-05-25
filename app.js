ed853390ce7e4d4950dd59747d2ce80ae88e7c25
C.J. Scott <132962397+Cctns1981@users.noreply.github.com>
2023-05-24 21:15:00 -0500

    addjava

---
+++ app.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let username = prompt ('What is your Name?');
+console.log(username);
+
+document.write(username)
+function gitName(){
+console.log('work')
+    let sport = prompt ('What is your favorite sport?')
+    document.write(sport)
+}
+console.log('happy')
